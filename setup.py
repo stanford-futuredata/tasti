1cf45b151f375431bff48552b317597a1a3516b3
John Taro Guibas <jtguibas@gmail.com>
2020-08-10 19:12:23 -0700

    remove double quotes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name = "TASTI",
-    version = "0.0.1",
-    author = "Daniel Kang",
-    author_email = "ddkang@stanford.edu",
+    name = 'TASTI',
+    version = '0.0.1',
+    author = 'Daniel Kang',
+    author_email = 'ddkang@stanford.edu',
     packages=setuptools.find_packages(),
-    python_requires=">=3.7",
+    python_requires='>=3.7',
 )
